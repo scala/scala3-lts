9d27448df4b66f973e04843bd5b4dada7b4af501
Som Snytt <som.snytt@gmail.com>
2025-03-15 11:05:45 -0700

    Revert unconditional lint of Inlined

--- compiler/src/dotty/tools/dotc/transform/CheckUnused.scala
+++ compiler/src/dotty/tools/dotc/transform/CheckUnused.scala
@@ -131,6 +131,7 @@ class CheckUnused private (phaseMode: PhaseMode, suffix: String) extends MiniPha
     refInfos.inlined.push(tree.call.srcPos)
     ctx
   override def transformInlined(tree: Inlined)(using Context): tree.type =
+    //transformAllDeep(tree.expansion) // traverse expansion with nonempty inlined stack to avoid registering defs
     val _ = refInfos.inlined.pop()
     if !tree.call.isEmpty && phaseMode.eq(PhaseMode.Aggregate) then
       transformAllDeep(tree.call)
