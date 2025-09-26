scala/scala3

Prevent opaque types leaking from transparent inline methods
Before this PR, every transparent inline call returning an opaque type
would actually be typed with an intersection type `DECLARED & ACTUAL`,
where `DECLARED` was the declared return type of the transparent method,
and `ACTUAL` was the type actually returned in the expansion, with every
opaque type alias then dealiased. There was no way to guard against this
dealiasing. With the changes in this PR, users are now able to manually
ensure that they receive the types they want, although they might have
to manually annotate the returned type inside of the transparent inline
method body (as described in the added documentation section).

The previous dealiasing was caused by the proxy mechanism in inlining,
which would effectively deals every opaque type, that is transparent
from the perspective of the original method declaration. Now, we try to
map the results of the transparent inline back to the original (opaque)
types.

However all of this is only true for the outermost transparent inline
method calls. Nested calls will not be affected by this change. This is
because the type checker in the original context of the method will see
the opaque type as transparent (so it will type the rest of the method
according to that), and that typing must still hold after inlining the
method e.g.:
```
object Time:
  opaque type Time = String
  transparent inline makeTime(): Time = "1h"
  transparent inline listTime(): List[Time] = List[String](makeTime())
    // mapping the results of makeTime() back into opaque types outside
    // of the scope of Time will cause an inlining compilation error
    // (which we are generally trying to avoid, and which would be
    // considered a bug in the compiler).
```
This might cause the aliased type to still leak in a manner that may
feel unexpected. In the above example, even if the List does not have
an explicit type parameter, the type inference will still decide on
`String`, causin

tests/run/i13461-b.scala
@@ -1,0 +1,60 @@
+// TODO taken from issue
+object Time:
+  opaque type Time = String
+  opaque type Seconds <: Time = String
+  opaque type Minutes <: Time = String
+  opaque type Mixed <: Time = String
+
+  type Units = Seconds | Minutes
+
+  def sec(n: Double): Seconds =
+    s"${n}s"
+
+  def min(n: Double): Minutes =
+    s"${n}m"
+
+  def mixed(t1: Time, t2: Time): Mixed =
+    s"${t1}, ${t2}"
+
+  extension (t: Units)
+    def number: Double =
+      (t : String).init.toDouble
+
+  extension [T1 <: Time](inline a: T1)
+    transparent inline def +[T2 <: Time](inline b: T2): Time =
+      inline (a, b) match
+        case x: (Seconds, Seconds) =>
+          (sec(x._1.number + x._2.number))
+
+        case x: (Minutes, Minutes) =>
+          (min(x._1.number + x._2.number))
+
+        case x: (Time, Time) =>
+          (mixed(x._1, x._2))
+    end +
+end Time
+
+import Time.*
+
+// Test seconds
+val a = sec(15)
+val b = sec(20)
+
+// Test minutes
+val x = min(15)
+val y = min(20)
+
+// Test mixes
+val m1 = a + y
+val m2 = x + b
+
+// Test upper type
+val t1: Time = a
+val t2: Time = x
+val t3: Time = m1
+
+@main def Test() =
+  println(a + b)
+  println(x + y)
+  println(m1 + m2)
+  println(t1 + t2 + t3)

scala/scala3-lts

tests/run/i13461-b.scala
@@ -1,0 +1,60 @@
+// TODO taken from issue
+object Time:
+  opaque type Time = String
+  opaque type Seconds <: Time = String
+  opaque type Minutes <: Time = String
+  opaque type Mixed <: Time = String
+
+  type Units = Seconds | Minutes
+
+  def sec(n: Int): Seconds =
+    s"${n}s"
+
+  def min(n: Int): Minutes =
+    s"${n}m"
+
+  def mixed(t1: Time, t2: Time): Mixed =
+    s"${t1}, ${t2}"
+
+  extension (t: Units)
+    def number: Int =
+      (t : String).init.toInt
+
+  extension [T1 <: Time](inline a: T1)
+    transparent inline def +[T2 <: Time](inline b: T2): Time =
+      inline (a, b) match
+        case x: (Seconds, Seconds) =>
+          (sec(x._1.number + x._2.number))
+
+        case x: (Minutes, Minutes) =>
+          (min(x._1.number + x._2.number))
+
+        case x: (Time, Time) =>
+          (mixed(x._1, x._2))
+    end +
+end Time
+
+import Time.*
+
+// Test seconds
+val a = sec(15)
+val b = sec(20)
+
+// Test minutes
+val x = min(15)
+val y = min(20)
+
+// Test mixes
+val m1 = a + y
+val m2 = x + b
+
+// Test upper type
+val t1: Time = a
+val t2: Time = x
+val t3: Time = m1
+
+@main def Test() =
+  println(a + b)
+  println(x + y)
+  println(m1 + m2)
+  println(t1 + t2 + t3)