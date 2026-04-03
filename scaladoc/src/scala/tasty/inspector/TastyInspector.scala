scala/scala3

Bring back the fix for scaladoc TastyInspector regressions
Brings back the previosuly reverted commit with an added
fix for the test.

scaladoc/src/scala/tasty/inspector/TastyInspector.scala
@@ -1,5 +1,7 @@
-// Copy of tasty-inspector/src/scala/tasty/inspector/TastyInspector.scala
+// Renamed copy of tasty-inspector/src/scala/tasty/inspector/TastyInspector.scala
 // FIXME remove this copy of the file
+// Since copying, an inspectAllTastyFilesInContext method was added for scaladoc only
+// to fix regressions introduced by the switch from old to a new TastyInspector
 
 package scala.tasty.inspector
 
@@ -21,7 +23,7 @@ import dotty.tools.dotc.report
 
 import java.io.File.pathSeparator
 
-object TastyInspector:
+object ScaladocInternalTastyInspector:
 
   /** Load and process TASTy files using TASTy reflect
    *
@@ -41,6 +43,32 @@ object TastyInspector:
   def inspectTastyFilesInJar(jar: String)(inspector: Inspector): Boolean =
     inspectAllTastyFiles(Nil, List(jar), Nil)(inspector)
 
+  private def checkFiles(tastyFiles: List[String], jars: List[String]): Unit =
+    def checkFile(fileName: String, ext: String): Unit =
+      val file = dotty.tools.io.Path(fileName)
+      if !file.ext.toLowerCase.equalsIgnoreCase(ext) then
+        throw new IllegalArgumentException(s"File extension is not `.$ext`: $file")
+      else if !file.exists then
+        throw new IllegalArgumentException(s"File not found: ${file.toAbsolute}")
+    tastyFiles.foreach(checkFile(_, "tasty"))
+    jars.foreach(checkFile(_, "jar"))
+
+  /**
+  * Added for Scaladoc-only.
+  * Meant to fix regressions introduces by the switch from old to new TastyInspector:
+  *  https://github.com/scala/scala3/issues/18231
+  *  https://github.com/scala/scala3/issues/20476
+  * Stable TastyInspector API does not support passing compiler context.
+  */
+  def inspectAllTastyFilesInContext(tastyFiles: List[String], jars: List[String], dependenciesClasspath: List[String])(inspector: Inspector)(using Context): Boolean =
+    checkFiles(tastyFiles, jars)
+    val classes = tastyFiles ::: jars
+    classes match
+      case Nil => true
+      case _ =>
+        val reporter = inspectorDriver(inspector).process(inspectorArgs(dependenciesClasspath, classes), summon[Context])
+        !reporter.hasErrors
+
   /** Load and process TASTy files using TASTy reflect
    *
    *  @param tastyFiles List of paths of `.tasty` files
@@ -50,14 +78,7 @@ object TastyInspector:
    *  @return boolean value indicating whether the process succeeded
    */
   def inspectAllTastyFiles(tastyFiles: List[String], jars: List[String], dependenciesClasspath: List[String])(inspector: Inspector): Boolean =
-    def checkFile(fileName: String, ext: String): Unit =
-      val file = dotty.tools.io.Path(fileName)
-      if !file.ext.toLowerCase.equalsIgnoreCase(ext) then
-        throw new IllegalArgumentException(s"File extension is not `.$ext`: $file")
-      else if !file.exists then
-        throw new IllegalArgumentException(s"File not found: ${file.toAbsolute}")
-    tastyFiles.foreach(checkFile(_, "tasty"))
-    jars.foreach(checkFile(_, "jar"))
+    checkFiles(tastyFiles, jars)
     val files = tastyFiles ::: jars
     inspectFiles(dependenciesClasspath, files)(inspector)
 
@@ -124,4 +145,4 @@ object TastyInspector:
   end inspectFiles
 
 
-end TastyInspector
+end ScaladocInternalTastyInspector

scala/scala3-lts

scaladoc/src/scala/tasty/inspector/TastyInspector.scala
@@ -1,5 +1,7 @@
-// Copy of tasty-inspector/src/scala/tasty/inspector/TastyInspector.scala
+// Renamed copy of tasty-inspector/src/scala/tasty/inspector/TastyInspector.scala
 // FIXME remove this copy of the file
+// Since copying, an inspectAllTastyFilesInContext method was added for scaladoc only
+// to fix regressions introduced by the switch from old to a new TastyInspector
 
 package scala.tasty.inspector
 
@@ -21,7 +23,7 @@ import dotty.tools.dotc.report
 
 import java.io.File.pathSeparator
 
-object TastyInspector:
+object ScaladocInternalTastyInspector:
 
   /** Load and process TASTy files using TASTy reflect
    *
@@ -41,6 +43,32 @@ object TastyInspector:
   def inspectTastyFilesInJar(jar: String)(inspector: Inspector): Boolean =
     inspectAllTastyFiles(Nil, List(jar), Nil)(inspector)
 
+  private def checkFiles(tastyFiles: List[String], jars: List[String]): Unit =
+    def checkFile(fileName: String, ext: String): Unit =
+      val file = dotty.tools.io.Path(fileName)
+      if !file.extension.toLowerCase.equalsIgnoreCase(ext) then
+        throw new IllegalArgumentException(s"File extension is not `.$ext`: $file")
+      else if !file.exists then
+        throw new IllegalArgumentException(s"File not found: ${file.toAbsolute}")
+    tastyFiles.foreach(checkFile(_, "tasty"))
+    jars.foreach(checkFile(_, "jar"))
+
+  /**
+  * Added for Scaladoc-only.
+  * Meant to fix regressions introduces by the switch from old to new TastyInspector:
+  *  https://github.com/scala/scala3/issues/18231
+  *  https://github.com/scala/scala3/issues/20476
+  * Stable TastyInspector API does not support passing compiler context.
+  */
+  def inspectAllTastyFilesInContext(tastyFiles: List[String], jars: List[String], dependenciesClasspath: List[String])(inspector: Inspector)(using Context): Boolean =
+    checkFiles(tastyFiles, jars)
+    val classes = tastyFiles ::: jars
+    classes match
+      case Nil => true
+      case _ =>
+        val reporter = inspectorDriver(inspector).process(inspectorArgs(dependenciesClasspath, classes), summon[Context])
+        !reporter.hasErrors
+
   /** Load and process TASTy files using TASTy reflect
    *
    *  @param tastyFiles List of paths of `.tasty` files
@@ -50,14 +78,7 @@ object TastyInspector:
    *  @return boolean value indicating whether the process succeeded
    */
   def inspectAllTastyFiles(tastyFiles: List[String], jars: List[String], dependenciesClasspath: List[String])(inspector: Inspector): Boolean =
-    def checkFile(fileName: String, ext: String): Unit =
-      val file = dotty.tools.io.Path(fileName)
-      if file.extension != ext then
-        throw new IllegalArgumentException(s"File extension is not `.$ext`: $file")
-      else if !file.exists then
-        throw new IllegalArgumentException(s"File not found: ${file.toAbsolute}")
-    tastyFiles.foreach(checkFile(_, "tasty"))
-    jars.foreach(checkFile(_, "jar"))
+    checkFiles(tastyFiles, jars)
     val files = tastyFiles ::: jars
     inspectFiles(dependenciesClasspath, files)(inspector)
 
@@ -124,4 +145,4 @@ object TastyInspector:
   end inspectFiles
 
 
-end TastyInspector
+end ScaladocInternalTastyInspector